479514082136a7eea59599d0255e36efc3660295
Ira <ira.kumpula@metropolia.fi>
2025-11-14 11:21:44 +0200

    Add valitation rules and sanitation, add test requests for error handlers, add error handlers

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -6,18 +6,33 @@ import {
   postCat,
   putCat,
   deleteCat,
-  getMyCats
+  getMyCats,
 } from '../controllers/cat-controller.js';
 import {upload} from '../../middlewares/upload.js';
+import {body} from 'express-validator';
 
 // Multer imports
-import {authenticateToken} from '../../middlewares/authentication.js'
+import {authenticateToken} from '../../middlewares/authentication.js';
+import { validationErrors } from '../../middlewares/error-handlers.js';
 
 // Cat router
 const catRouter = express.Router();
 
-// Upload image
-catRouter.route('/').get(getCat).post(authenticateToken, upload.single('file'), postCat);   // use upload from upload.js
+// Upload new cat with image with validation rules and sanitation, e.g. name must be 3-128 digits.
+// File needs to be uploaded before validation rules so body exists.
+catRouter
+  .route('/')
+  .get(getCat)
+  .post(
+    authenticateToken,
+    upload.single('file'),
+    body('cat_name').trim().isLength({min: 3, max: 128}).escape(),
+    body('weight').trim().isNumeric().toFloat(),
+    body('owner').trim().isInt(),
+    body('birthdate').trim().isDate,
+    validationErrors,
+    postCat
+  ); // use upload from upload.js, validate cat input
 
 // Get own cats
 catRouter.route('/user').get(authenticateToken, getMyCats);
--- src/api/routes/user-router.js
+++ src/api/routes/user-router.js
@@ -7,12 +7,23 @@ import {
   deleteUser,
 } from '../controllers/user-controller.js';
 import {authenticateToken} from '../../middlewares/authentication.js';
+import {body} from 'express-validator';
+import {validationErrors} from '../../middlewares/error-handlers.js';
 
 // User router
 const userRouter = express.Router();
 
 // Require token to get all users
-userRouter.route('/').get(authenticateToken, getUser).post(postUser);
+//userRouter.route('/').get(authenticateToken, getUser).post(postUser);
+
+// Get all users, require token and validate
+userRouter.route('/').get(getUser).post(
+    body('email').trim().isEmail(),
+    body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
+    body('password').trim().isLength({min: 8}),
+    validationErrors,
+    postUser
+  );
 
 // Get user by id
 userRouter.route('/:id').get(getUserById).put(putUser).delete(deleteUser);
